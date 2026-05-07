076edf6cfa0e52ee6787a26dbb0dcfd6ae3961cc
juliarci <123546836+juliarci@users.noreply.github.com>
2026-05-07 11:34:48 +0200

    Component name change

--- tp-angular-bureau-des-entrees/src/app/component-name/component-name.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-component-name',
-  imports: [],
-  templateUrl: './component-name.html',
-  styleUrl: './component-name.css',
-})
-export class ComponentName {}
