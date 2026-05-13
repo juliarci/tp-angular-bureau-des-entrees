edc40df19d78911c8ce59c851e359429b3e5414f
juliarci <123546836+juliarci@users.noreply.github.com>
2026-05-13 16:28:15 +0200

    Fix patient.service.ts to prepare merging

--- tp-angular-bureau-des-entrees/src/app/services/patient.service.ts
+++ tp-angular-bureau-des-entrees/src/app/services/patient.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { BureauEntreesPatient } from '../models/patient.model';
+import {Observable} from 'rxjs';
+import {Patient} from 'fhir/r4';
+import {Bundle} from 'fhir/r2';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +20,28 @@ export class PatientService {
    */
   createPatient(patient: BureauEntreesPatient) {
     return this.http.post<BureauEntreesPatient>(this.apiUrl, patient);
+  }
+
+  searchPatients(criteria: { name?: string; identifier?: string; birthdate?: string }): Observable<Patient[]> {
+    let params = new HttpParams();
+
+    if (criteria.name) {
+      params = params.set('name', criteria.name);
+    }
+    if (criteria.identifier) {
+      params = params.set('identifier', criteria.identifier);
+    }
+    if (criteria.birthdate) {
+      params = params.set('birthdate', criteria.birthdate);
+    }
+
+    return this.http.get<Bundle>(`${this.apiUrl}/Patient`, { params }).pipe(
+      map(bundle => {
+        if (!bundle.entry) return [];
+        return bundle.entry
+          .filter(e => (e.resource as any)?.resourceType === 'Patient')
+          .map(e => e.resource as Patient);
+      })
+    );
   }
 }
