93f3cce24d37fc8df947ef9fbf96e4b96bd5f308
dizida <dizida1@gmail.com>
2026-05-14 11:54:09 +0200

    Résolution de la suppression du fichier practiotioner

--- tp-angular-bureau-des-entrees/src/app/services/practitioner.service.ts
+++ tp-angular-bureau-des-entrees/src/app/services/practitioner.service.ts
@@ -2,33 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Bundle, Practitioner } from 'fhir/r4';  // ← tout depuis fhir/r4, pas fhir/r2
-import { environment } from '../../environments/environment';
+import { Bundle, Practitioner } from 'fhir/r4';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PractitionerService {
 
-  private apiUrl = `${environment.apiUrl}/Practitioner`;  // ← une seule apiUrl
+  private apiUrl = 'https://fhir.chl.connected-health.fr/fhir';
 
   constructor(private http: HttpClient) {}
 
   // Récupère un médecin par son ID
   getPractitioner(practitionerId: string): Observable<Practitioner> {
-    return this.http.get<Practitioner>(`${this.apiUrl}/${practitionerId}`);
+    return this.http.get<Practitioner>(`${this.apiUrl}/Practitioner/${practitionerId}`);
   }
 
   // Récupère tous les médecins
   getAllPractitioners(): Observable<Practitioner[]> {
-    return this.http.get<Bundle>(this.apiUrl).pipe(
+    return this.http.get<Bundle>(`${this.apiUrl}/Practitioner`).pipe(
       map(bundle => {
         if (!bundle.entry) return [];
         return bundle.entry
-          .filter((e): e is { resource: Practitioner } =>
-            e.resource !== undefined && e.resource.resourceType === 'Practitioner'
-          )
-          .map(e => e.resource);
+          .filter((e: any) => (e.resource as any)?.resourceType === 'Practitioner')
+          .map((e: any) => e.resource as Practitioner);
       })
     );
   }
