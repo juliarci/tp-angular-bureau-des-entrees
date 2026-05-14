c2789c903707086c21741cb440c61eb5378bb4f7
dizida <dizida1@gmail.com>
2026-05-14 11:47:50 +0200

    Résolution de la suppression du fichier practiotioner

---
+++ tp-angular-bureau-des-entrees/src/app/services/practitioner.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Bundle, Practitioner } from 'fhir/r4';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PractitionerService {
+  private apiUrl = 'https://fhir.chl.connected-health.fr/fhir';
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Récupère un médecin par son ID
+   */
+  getPractitioner(practitionerId: string): Observable<Practitioner> {
+    return this.http.get<Practitioner>(`${this.apiUrl}/Practitioner/${practitionerId}`);
+  }
+
+  /**
+   * Récupère tous les médecins
+   */
+  getAllPractitioners(): Observable<Practitioner[]> {
+    return this.http.get<Bundle>(`${this.apiUrl}/Practitioner`).pipe(
+      map(bundle => {
+        if (!bundle.entry) return [];
+        return bundle.entry
+          .filter((e: any) => (e.resource as any)?.resourceType === 'Practitioner')
+          .map((e: any) => e.resource as Practitioner);
+      })
+    );
+  }
+}
